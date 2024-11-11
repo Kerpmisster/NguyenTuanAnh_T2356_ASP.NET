dbd1b8cb8b6b3277afd5d12a76af78bd3012fa65
Kerpmisster <nta20030123@gmail.com>
2024-11-11 20:21:04 +0700

    LESSON01 and Lesson02

---
+++ LESSON01/LESSON01/Controllers/AccountController.cs
@@ -0,0 +1,103 @@
+﻿using LESSON01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LESSON01.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Account> accounts = new List<Account>
+            {
+                new Account()
+                {
+                    Id = 1,
+                    Name = "Hoàng Anh",
+                    Email = "anh@gmail.com",
+                    Phone = "0986456789",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/images/avatar/avatar1.png"),
+                    Gender = 1,
+                    Bio = "My profile",
+                    Birthday = new DateTime(2003,7,15)
+                },
+
+                new Account()
+                {
+                    Id = 2,
+                    Name = "Hoàng Anh",
+                    Email = "anh@gmail.com",
+                    Phone = "0986456789",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/images/avatar/avatar1.png"),
+                    Gender = 1,
+                    Bio = "My profile",
+                    Birthday = new DateTime(2003,7,15)
+                },
+
+                new Account()
+                {
+                    Id = 3,
+                    Name = "Hoàng Anh",
+                    Email = "anh@gmail.com",
+                    Phone = "0986456789",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/images/avatar/avatar1.png"),
+                    Gender = 1,
+                    Bio = "My profile",
+                    Birthday = new DateTime(2003,7,15)
+                }
+            };
+            ViewBag.Accounts = accounts;
+            return View();
+        }
+        [Route("ho-so-cua-toi", Name ="profile")]
+        public IActionResult Profile(int id)
+        {
+            List<Account> accounts = new List<Account>
+            {
+                new Account()
+                {
+                    Id = 1,
+                    Name = "Hoàng Anh",
+                    Email = "anh@gmail.com",
+                    Phone = "0986456789",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/images/avatar/avatar1.png"),
+                    Gender = 1,
+                    Bio = "My profile",
+                    Birthday = new DateTime(2003,7,15)
+                },
+
+                new Account()
+                {
+                    Id = 2,
+                    Name = "Hoàng Anh",
+                    Email = "anh@gmail.com",
+                    Phone = "0986456789",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/images/avatar/avatar1.png"),
+                    Gender = 1,
+                    Bio = "My profile",
+                    Birthday = new DateTime(2003,7,15)
+                },
+
+                new Account()
+                {
+                    Id = 3,
+                    Name = "Hoàng Anh",
+                    Email = "anh@gmail.com",
+                    Phone = "0986456789",
+                    Address = "Hà Nội",
+                    Avatar = Url.Content("~/images/avatar/avatar1.png"),
+                    Gender = 1,
+                    Bio = "My profile",
+                    Birthday = new DateTime(2003,7,15)
+                }
+            };
+            Account account = accounts.FirstOrDefault()
+            ViewBag.Account = account;
+            return View();
+        }
+    }
+}
---
+++ LESSON01/LESSON01/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LESSON01.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
